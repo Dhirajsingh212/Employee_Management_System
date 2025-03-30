b8a4d1aa7e9529b6de3eda169615ca2c631436e5
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2025-03-30 16:23:58 +0530

    minor ui updates

---
+++ ems-client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+export default function Navbar(){
+    return (
+        <div className='flex flex-row justify-center py-3 border-b-2 border-violet-200'>
+            <p className='text-2xl font-semibold text-violet-500'>
+                Employee Management System
+            </p>
+        </div>
+    )
+}
